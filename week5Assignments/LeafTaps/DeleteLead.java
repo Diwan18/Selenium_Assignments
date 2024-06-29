77e200d353863114c8ac50f39b1dabdbb41cccab
Mohammad Peer Diwan <89515903+Diwan18@users.noreply.github.com>
2024-06-29 12:47:57 +0530

    Update DeleteLead.java

--- week5Assignments/LeafTaps/DeleteLead.java
+++ week5Assignments/LeafTaps/DeleteLead.java
@@ -1,19 +1,39 @@
 package week5Assignments.LeafTaps;
 
+import java.io.IOException;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class DeleteLead extends BaseClass {
-	@Test
-	public  void DeleteLead()  {
+	
+	
+	
+	
+	
+	@DataProvider(name="getValue")
+	public String[][] data() throws IOException{
+		
+
+		
+		return ExcelSheet2.readexcel();
+		
+		
+		
+	}
+	
+	
+	
+	@Test(dataProvider="getValue")
+	public  void ToDeleteLead(String phNo)  {
 		
 		driver.findElement(By.linkText("Leads")).click();
 		driver.findElement(By.linkText("Find Leads")).click();
 		driver.findElement(By.xpath("//span[text()='Phone']")).click();
-		driver.findElement(By.xpath("//input[@name='phoneNumber']")).sendKeys("99");
+		driver.findElement(By.xpath("//input[@name='phoneNumber']")).sendKeys(phNo);
 		driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
 		try {
 			Thread.sleep(2000);
