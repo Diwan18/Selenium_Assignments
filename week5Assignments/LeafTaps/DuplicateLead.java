6e2546969bb86e9eaea610ce8b3ec22181cc5e9c
Mohammad Peer Diwan <89515903+Diwan18@users.noreply.github.com>
2024-06-29 12:48:22 +0530

    Update DuplicateLead.java

--- week5Assignments/LeafTaps/DuplicateLead.java
+++ week5Assignments/LeafTaps/DuplicateLead.java
@@ -1,24 +1,37 @@
 package week5Assignments.LeafTaps;
 
+import java.io.IOException;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 
 
 public class DuplicateLead extends BaseClass {
 	
+	@DataProvider(name="getValue")
+	public String[][] data() throws IOException{
+		
 
-	@Test
-	public  void duplicateLead() throws InterruptedException {
+		
+		return ExcelSheet2.readexcel();
+		
+		
+		
+	}
+	
+
+	@Test(dataProvider="getValue")
+	public  void duplicateLead(String phNo) throws InterruptedException {
 		
 
 		driver.findElement(By.linkText("Leads")).click();
 		driver.findElement(By.linkText("Find Leads")).click();
 		driver.findElement(By.xpath("//span[text()='Phone']")).click();
-		driver.findElement(By.xpath("//input[@name='phoneNumber']")).sendKeys("99");
+		driver.findElement(By.xpath("//input[@name='phoneNumber']")).sendKeys(phNo);
 		driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
 		Thread.sleep(2000);
 		driver.findElement(By.xpath("//div[@class='x-grid3-cell-inner x-grid3-col-partyId']/a")).click();
